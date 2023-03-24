91ffb2acba06be9af21b892df4a145605b00b21d
KauaPrado <kauaprado@live.com>
2023-03-24 00:43:21 -0300

    finalizado

--- app.py
+++ app.py
@@ -52,4 +52,24 @@ def editar_livro_por_id(id):
             livros[indice].update(livro_alterado)
             return jsonify(livros[indice])
 
+# criar
+
+@app.route('/livros', methods =['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+
+    return jsonify(livros)
+
+
+# excluir
+@app.route('/livros/<int:id>', methods =['DELETE'])
+def excluir_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+
+    return jsonify(livros)
+
+
 app.run(port=5000,host='localhost', debug=True)
